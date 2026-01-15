74c48ecd57319a77eacdfef01d614f424acce82a
Necony286 (Nemanja.B.) <necony286@gmail.com>
2026-01-15 07:46:45 +0100

    Fix React runtime imports in quiz components

--- src/components/figma/quiz/QuizHeader.tsx
+++ src/components/figma/quiz/QuizHeader.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import { ProgressBar } from '@/components/figma/ProgressBar';
 
 interface QuizHeaderProps {
--- src/components/figma/quiz/StickyNavigation.tsx
+++ src/components/figma/quiz/StickyNavigation.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { ChevronLeft, ChevronRight } from 'lucide-react';
 
 interface StickyNavigationProps {

8ac6a75ab2240a25bc847b88b3f22392284bc40a
Necony286 (Nemanja.B.) <necony286@gmail.com>
2026-01-18 17:38:25 +0100

    Expand Paddle customer types

--- types/paddle.d.ts
+++ types/paddle.d.ts
@@ -5,8 +5,24 @@ type PaddleCheckoutItem = {
   quantity?: number;
 };
 
+type PaddleAddress = {
+  addressLine1?: string;
+  addressLine2?: string;
+  city?: string;
+  countryCode?: string;
+  postalCode?: string;
+  region?: string;
+};
+
+type PaddleBusiness = {
+  name?: string;
+  taxIdentifier?: string;
+};
+
 type PaddleCustomer = {
   email?: string;
+  address?: PaddleAddress;
+  business?: PaddleBusiness;
 };
 
 type PaddleCheckoutOptions = {

ed73b474f6654d4b6d030680528569c394d9cdc7
Necony286 (Nemanja.B.) <necony286@gmail.com>
2026-01-21 06:55:06 +0100

    Add a11y checks for core screens

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,3 +1,7 @@
 import '@testing-library/jest-dom';
+import { toHaveNoViolations } from 'jest-axe';
+import { expect } from 'vitest';
 
 process.env.REPORT_ACCESS_TOKEN_PEPPER ??= 'test-report-access-pepper';
+
+expect.extend(toHaveNoViolations);
